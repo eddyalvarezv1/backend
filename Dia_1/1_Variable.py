b96ecf4ca4481a6095a06e54d551ffd1c29ef730
Eddy Alvarez <eddyalvarezvillavicencio@gmail.com>
2024-11-14 21:03:54 -0500

    inicios en python

---
+++ Dia_1/1_Variable.py
@@ -0,0 +1,33 @@
+#Tipos de datos de variables primitivas
+
+#String
+nombre = "Eddy"
+direccion = 'Las lechuzas "512'
+
+#Cuando queremos guardar un texto muy largo que ademas puede tener saltos de linea
+enunciado = '''Hola
+como estas
+        Te extraño
+Espero verte pronto,
+
+Saludos
+
+Eduardo '''
+
+otro_enunciado = """Hola
+como estas
+        Te extraño
+Espero verte pronto,
+
+Saludos
+
+Eduardo"""
+
+#Int
+edad = 32
+
+#Boolean
+casado = True
+
+#Float
+estatura = 1.82
